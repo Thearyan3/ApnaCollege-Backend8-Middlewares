d5724bb47ac746490a4e318f592284bc53f8c937
Aryan <chaudharyaryan3456@gmail.com>
2025-06-29 20:16:48 +0530

    Learning Middlewares

--- app.js
+++ app.js
@@ -11,21 +11,33 @@ const app = express();
 //     next();
 // });
 
-app.use((req, res, next) => {
-    console.log("I am only for random");
-    next();
+const checkToken =  (req, res, next) => {
+    let {token} = req.query;
+    if(token === "give access"){
+        next();
+    }
+    res.send("ACCESS DENIED!");
+};
+
+app.get("/api", checkToken, (req, res) => {
+    res.send("data");
 });
 
-app.use((req, res) => {
-    res.send("Page not found!");
-});
+// app.use((req, res, next) => {
+//     console.log("I am only for random");
+//     next();
+// });
+
+// app.use((req, res) => {
+//     res.send("Page not found!");
+// });
 
 //logger - morgan(One of the famous and necessary middleware that every developer should know)
-app.use((req, res, next) => {
-    req.time = new Date(Date.now()).toString();
-    console.log(req.method ,req.hostname, req.path, req.time);
-    next();
-});
+// app.use((req, res, next) => {
+//     req.time = new Date(Date.now()).toString();
+//     console.log(req.method ,req.hostname, req.path, req.time);
+//     next();
+// });
 
 app.get("/", (req, res) => {
     res.send("Root is working");
