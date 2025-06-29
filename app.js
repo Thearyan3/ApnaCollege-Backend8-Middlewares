6d9a3c2e38d6e38d21137209969cc06b8154dbf9
Aryan <chaudharyaryan3456@gmail.com>
2025-06-29 20:01:44 +0530

    Learning Middlewares

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("Hi, i am 1st middleware");
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log("Hi, i am 1st middleware");
+//     next();
+// });
 
+// app.use((req, res, next) => {
+//     console.log("Hi, i am 2nd middleware");
+//     next();
+// });
+
+//logger - morgan(One of the famous and necessary middleware that every developer should know)
 app.use((req, res, next) => {
-    console.log("Hi, i am 2nd middleware");
+    req.time = new Date(Date.now()).toString();
+    console.log(req.method ,req.hostname, req.path, req.time);
     next();
 });
 
